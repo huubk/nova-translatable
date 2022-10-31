69bd393f6f1c5269b3bc8aa1b833b36063c07cc2
HuubK <huub@soicca.com>
2022-10-31 16:51:52 +0100

    added row-left display type

--- resources/js/components/LocaleTabs.vue
+++ resources/js/components/LocaleTabs.vue
@@ -30,7 +30,7 @@ export default {
   computed: {
     listClasses() {
       if (this.displayType === 'column') return ['ml-0','flex', 'flex-col'];
-      if (this.displayType === 'row-left') return ['w-full md:w-3/5'];
+      if (this.displayType === 'row') return ['w-full md:w-3/5'];
       return ['ml-0'];
     },
   },
